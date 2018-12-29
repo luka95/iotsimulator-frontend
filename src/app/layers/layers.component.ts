38286359138dba7cdeff1189ec78347263d2214d
Luka Jurić <luka.juric@fer.hr>
2018-12-29 15:57:59 +0100

    adjust marker icon fill according to modules that node possesses

--- src/app/layers/layers.component.ts
+++ src/app/layers/layers.component.ts
@@ -45,22 +45,6 @@ export class LayersComponent {
     'Open Topo Map': this.LAYER_OTM.layer
   };
 
-  // TODO dynamically fetch fill color for elements with id == "Left" and "Right"
-  mySvgString = `<svg width="20px" height="24px" viewBox="0 0 20 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                    <metadata id="metadata1">image/svg+xml</metadata>
-                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <g id="map-pin" transform="translate(1.000000, 1.000000)" stroke="#000000">
-                        <path d="M9,21.9999999 C9,21.9999999 0,15.9999999 0,8.99999987 C7.40671569e-08,4.02943717 4.0294373,-2.22044605e-16 9,0 C13.9705627,2.22044605e-16 17.9999999,4.02943717 18,8.99999987 C18,15.9999999 9,21.9999999 9,21.9999999 Z" id="Path" stroke-width="0.7" fill="#48EF18" stroke-linecap="round" stroke-linejoin="round"/>
-                        <g id="Indicator" transform="translate(2.000000, 4.000000)" stroke-width="0.4">
-                          <path d="M0,0 L4,0 C5.65685425,-3.04359188e-16 7,1.34314575 7,3 L7,3 C7,4.65685425 5.65685425,6 4,6 L0,6" id="Left" fill="#3F3665" transform="translate(3.500000, 3.000000) scale(-1, 1) translate(-3.500000, -3.000000) "/>
-                          <path d="M7,0 L11,0 C12.6568542,-3.04359188e-16 14,1.34314575 14,3 L14,3 C14,4.65685425 12.6568542,6 11,6 L7,6" id="Right" fill="#864545"/>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>`;
-
-  myIconUrl = encodeURI('data:image/svg+xml,' + this.mySvgString).replace('#', '%23');
-
   numberOfLayers = 0;
 
   drawOptions = {
@@ -70,7 +54,7 @@ export class LayersComponent {
         icon: L.icon({
           iconSize: [ 20, 24 ],
           iconAnchor: [ 10, 24 ],
-          iconUrl: this.myIconUrl
+          iconUrl: this.getMarkerIcon()
         })
       },
       polyline: true,
@@ -79,15 +63,16 @@ export class LayersComponent {
     }
   };
 
-  onDrawStart(event) {
-    console.log(event);
-  }
-
   onDrawCreated(event) {
     const type = event.layerType;
-    event.layer = event.layer.toGeoJSON();
 
     if (type === 'marker') {
+      event.layer.setIcon(L.icon({
+        iconSize: [ 20, 24 ],
+        iconAnchor: [ 10, 24 ],
+        iconUrl: this.getMarkerIcon()
+      }));
+
       const availableModules = [];
 
       if (this.sensorFormComponent.getLoraWanSelectedStatus()) {
@@ -98,6 +83,7 @@ export class LayersComponent {
         availableModules.push(1);
       }
 
+      event.layer = event.layer.toGeoJSON();
       event.layer.properties = {
         id: this.numberOfLayers++,
         availableModules: availableModules,
@@ -113,7 +99,51 @@ export class LayersComponent {
     if (type === 'circle') {
       event.layer.geometry.type = 'Circle';
     }
+  }
 
-    console.log(event.layer);
+  getMarkerFill(id: String) {
+    const loraColor = '#3F3665';
+    const xbeeColor = '#864545';
+
+    if (this.sensorFormComponent === undefined) {
+      return 'none';
+    }
+
+    const lora = this.sensorFormComponent.getLoraWanSelectedStatus();
+    const xbee = this.sensorFormComponent.getXbeeSelectedStatus();
+    let colorToReturn = '';
+
+    if (lora === true && xbee === true) {
+      if (id === 'Left') {
+        colorToReturn = loraColor;
+      } else if (id === 'Right') {
+        colorToReturn = xbeeColor;
+      }
+    } else if (lora === true || xbee === true) {
+      colorToReturn = lora === true ? loraColor : xbeeColor;
+    } else {
+      colorToReturn = 'none';
+    }
+    return colorToReturn;
+  }
+
+  getMarkerIcon() {
+    const leftFill = this.getMarkerFill('Left');
+    const rightFill = this.getMarkerFill('Right');
+
+    const mySvgString = `<svg width="20px" height="24px" viewBox="0 0 20 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <metadata id="metadata1">image/svg+xml</metadata>
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="map-pin" transform="translate(1.000000, 1.000000)" stroke="#000000">
+                        <path d="M9,21.9999999 C9,21.9999999 0,15.9999999 0,8.99999987 C7.40671569e-08,4.02943717 4.0294373,-2.22044605e-16 9,0 C13.9705627,2.22044605e-16 17.9999999,4.02943717 18,8.99999987 C18,15.9999999 9,21.9999999 9,21.9999999 Z" id="Path" stroke-width="0.7" fill="#48EF18" stroke-linecap="round" stroke-linejoin="round"/>
+                        <g id="Indicator" transform="translate(2.000000, 4.000000)" stroke-width="0.4">
+                          <path d="M0,0 L4,0 C5.65685425,-3.04359188e-16 7,1.34314575 7,3 L7,3 C7,4.65685425 5.65685425,6 4,6 L0,6" id="Left" fill="` + leftFill + `" transform="translate(3.500000, 3.000000) scale(-1, 1) translate(-3.500000, -3.000000) "/>
+                          <path d="M7,0 L11,0 C12.6568542,-3.04359188e-16 14,1.34314575 14,3 L14,3 C14,4.65685425 12.6568542,6 11,6 L7,6" id="Right" fill="` + rightFill + `"/>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>`;
+
+    return encodeURI('data:image/svg+xml,' + mySvgString).replace('#', '%23');
   }
 }
