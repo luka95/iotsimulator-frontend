2378f59b6c32f0c57e41af78570056d9b3fb31da
Luka Jurić <luka.juric@infinum.hr>
2019-01-19 18:45:20 +0100

    enable configuration upload and obstacles and net drawing on map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login';
-import { RegisterComponent } from './register';
-import { AuthenticationService, LayersDataService, ModulesDataService, UserService, SimulationService } from './_services';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
-import { Ng2TableModule } from 'ng2-table/ng2-table';
-import { PaginationModule } from 'ng2-bootstrap/pagination';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoginComponent} from './login';
+import {RegisterComponent} from './register';
+import {AuthenticationService, LayersDataService, ModulesDataService, SimulationService, UserService} from './_services';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HeaderComponent} from './header';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {LeafletDrawModule} from '@asymmetrik/ngx-leaflet-draw';
+import {Ng2TableModule} from 'ng2-table/ng2-table';
+import {PaginationModule} from 'ng2-bootstrap/pagination';
 
 
-
-import { AppComponent } from './app.component';
-import { SensorFormComponent } from './sensor-form/sensor-form.component';
-import { routing } from './app.routing';
-import { LayersComponent } from './layers/layers.component';
-import { ErrorInterceptor, JwtInterceptor } from './_helpers';
-import { AuthGuard } from './_guards';
-//import {MarkerIconComponent} from './marker-icon/marker-icon.component';
-import { ObstacleFormComponent } from './obstacle-from/obstacle-form.component';
-import { SimulationFormComponent } from './simulation-form/simulation-form.component';
-import { ModulesFormComponent } from './modules-form/modules-form.component';
-import { SimulationsComponent } from './simulations/simulations.component';
+import {AppComponent} from './app.component';
+import {SensorFormComponent} from './sensor-form/sensor-form.component';
+import {routing} from './app.routing';
+import {LayersComponent} from './layers/layers.component';
+import {ErrorInterceptor, JwtInterceptor} from './_helpers';
+import {AuthGuard} from './_guards';
+import {ObstacleFormComponent} from './obstacle-from/obstacle-form.component';
+import {SimulationFormComponent} from './simulation-form/simulation-form.component';
+import {ModulesFormComponent} from './modules-form/modules-form.component';
+import {SimulationsComponent} from './simulations/simulations.component';
 
 
 @NgModule({
@@ -34,7 +32,6 @@ import { SimulationsComponent } from './simulations/simulations.component';
         LoginComponent,
         RegisterComponent,
         HeaderComponent,
-        //MarkerIconComponent,
         ObstacleFormComponent,
         SimulationFormComponent,
         ModulesFormComponent,
